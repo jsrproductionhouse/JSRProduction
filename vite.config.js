48f4fdf9f5ab440f517bb91052bf0972407418f4
Hridey <hridey79@gmail.com>
2025-06-25 16:01:51 +0530

    fixed meta tags

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import prerender from "vite-plugin-prerender";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    prerender({
+      staticDir: "dist",
+      routes: ["/"], // Add all routes you want to pre-render
+    }),
+  ],
+});
