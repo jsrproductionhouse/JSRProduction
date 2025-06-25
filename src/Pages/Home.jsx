84da6cd48b08388cb540202854b3d6413b750f7c
Hridey <hridey79@gmail.com>
2025-06-25 14:41:59 +0530

    SEO setup

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -12,6 +12,7 @@ import About from "../Components/About";
 import Gallery from "../Components/Gallery/Gallery";
 import Ventures from "../Components/Ventures";
 import Gallery2 from "../Components/Gallery/Gallery2";
+import { Helmet } from "react-helmet";
 
 const Home = () => {
   useEffect(() => {
@@ -20,9 +21,23 @@ const Home = () => {
 
   return (
     <div className="w-full h-full">
-      {/* <div className="absolute h-52 w-32 flex-col"> */}
+      {/* ✅ SEO Helmet */}
+      <Helmet>
+        <title>
+          JSR Production House | Top Film Production & Equipment Rental in
+          Dehradun, Uttarakhand
+        </title>
+        <meta
+          name="description"
+          content="Leading film production house in Dehradun offering video production & equipment rental services across Uttarakhand. We create films, corporate videos, ads, music videos & more with top-notch quality."
+        />
+      </Helmet>
       <div className="fixed top-[320px] lg:top-[260px] right-0 h-72 w-10 items-center flex flex-col gap-10 justify-center bg-gray-200 opacity-70 z-50 rounded-tl-md rounded-bl-md    ">
-        <a href="https://www.facebook.com/JSRProductionhouse" target="_blank">
+        <a
+          href="https://www.facebook.com/JSRProductionhouse"
+          target="_blank"
+          rel="noreferrer"
+        >
           <img
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/2021_Facebook_icon.svg/2048px-2021_Facebook_icon.svg.png"
             alt=""
@@ -32,6 +47,7 @@ const Home = () => {
         <a
           href="https://www.instagram.com/jsrproductionhouse/?igshid=YmMyMTA2M2Y%3D"
           target="_blank"
+          rel="noreferrer"
         >
           <img
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Instagram-Icon.png/1025px-Instagram-Icon.png"
@@ -39,7 +55,11 @@ const Home = () => {
             className="w-7 h-7 hover:scale-125  duration-200"
           />
         </a>
-        <a href="https://www.youtube.com/@jsrproductionhouse9127" target="_blank">
+        <a
+          href="https://www.youtube.com/@jsrproductionhouse9127"
+          target="_blank"
+          rel="noreferrer"
+        >
           <img
             src="https://cdn-icons-png.flaticon.com/512/1384/1384060.png"
             alt=""
@@ -48,7 +68,13 @@ const Home = () => {
         </a>
         <a href="https://twitter.com/house_jsr?lang=en">
           {/* <BsTwitter className="w-8 h-8 text-blue-500 hover:scale-125  duration-200" /> */}
-          <svg xmlns="http://www.w3.org/2000/svg" height="1.5em" viewBox="0 0 512 512"><path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"/></svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="1.5em"
+            viewBox="0 0 512 512"
+          >
+            <path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z" />
+          </svg>
         </a>
       </div>
       {/* </div> */}
